60ca09b8fa633b7f66e2a2216a9e47099c4f57af
Yeeswanth <yeeswanth.thiranx@gmail.com>
2026-07-21 22:06:48 +0530

    frontend added

--- frontend/widget.js
+++ frontend/widget.js
@@ -6,11 +6,17 @@
   }
 
   var targetUrl = scriptTag.getAttribute("data-url");
-  var apiBase = scriptTag.src.replace(/\/widget\.js.*$/, "");
+  var assetBase = scriptTag.src.replace(/\/widget\.js.*$/, "");
+  var apiBase = scriptTag.getAttribute("data-api-base") || assetBase;
 
-  import(apiBase + "/widget/index.js")
+  import(assetBase + "/widget/index.js")
     .then(function (mod) {
-      mod.initWidget({ targetUrl: targetUrl, apiBase: apiBase, scriptTag: scriptTag });
+      mod.initWidget({
+        targetUrl: targetUrl,
+        apiBase: apiBase,
+        assetBase: assetBase,
+        scriptTag: scriptTag
+      });
     })
     .catch(function (err) {
       console.error("[AeoGeoWidget] Failed to load widget module:", err);
--- frontend/widget/index.js
+++ frontend/widget/index.js
@@ -1,13 +1,13 @@
 import { fetchAnalysis } from "./apiClient.js";
 import { renderLoading, renderResult, renderError } from "./uiRenderer.js";
 
-export async function initWidget({ targetUrl, apiBase, scriptTag }) {
+export async function initWidget({ targetUrl, apiBase, assetBase, scriptTag }) {
   if (!targetUrl) {
     console.error("[AeoGeoWidget] Missing data-url attribute on script tag.");
     return;
   }
 
-  const container = renderLoading(scriptTag, apiBase);
+  const container = renderLoading(scriptTag, assetBase);
 
   try {
     const result = await fetchAnalysis(apiBase, targetUrl);
